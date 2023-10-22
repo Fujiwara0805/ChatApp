f96ed9ba9bf128dc1de5307ec90c827742864d6c
Fujiwara0805 <tiki4091@gmail.com>
2023-10-22 11:33:09 +0900

    Install expo-router

---
+++ src/app/ChatList.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+
+const ChatList = (): JSX.Element => {
+  return (
+    <View>
+      <View>
+        <Text>Hello World </Text>
+      </View>
+    </View>
+  )
+}
+
+export default ChatList
