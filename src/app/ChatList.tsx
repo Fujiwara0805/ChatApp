f5c2f3e398aad6a776a9be8ea1b49c7a28ecc70f
Fujiwara0805 <tiki4091@gmail.com>
2023-10-22 13:37:32 +0900

    Mark up Chat List Screen

--- src/app/ChatList.tsx
+++ src/app/ChatList.tsx
@@ -1,13 +1,41 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 const ChatList = (): JSX.Element => {
   return (
-    <View>
+    <View style={styles.container}>
       <View>
-        <Text>Hello World </Text>
+        <View>
+          <View>
+            <Text>Chat App </Text>
+            <Text>Log Out </Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <Text>日本経済について</Text>
+            <Text>2023年10月21日</Text>
+          </View>
+          <View>
+            <Text>✖️</Text>
+          </View>
+        </View>
       </View>
+
+      <View>
+        <Text>ボタン</Text>
+      </View>
+
     </View>
   )
 }
 
 export default ChatList
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
